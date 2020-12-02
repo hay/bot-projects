c56088e45c1b75bd2fcc2decfc1987dcb0d384af
Hay Kranen <huskyr@gmail.com>
2020-12-02 16:53:16 +0100

    Adding scraper/parser for pages

--- scrape-reliwiki.py
+++ scrape-reliwiki.py
@@ -1,8 +1,16 @@
-from util.mediawiki import AllPages
+from bs4 import BeautifulSoup
 from dataknead import Knead
+from pathlib import Path
+from time import sleep
+from util.mediawiki import AllPages
+from util.utils import dd
+import json
 import re
+import requests
 import sys
 
+API_ENDPOINT = "http://reliwiki.nl/api.php"
+RMM_ID = re.compile("https://monumentenregister.cultureelerfgoed.nl/monumenten/(\d{1,6})")
 TITLE = re.compile("([^,]+),([^-–]+)[-|–](.+)")
 
 def create_catalog():
@@ -23,6 +31,73 @@ def create_catalog():
         catalog.write(line)
 
     catalog.close()
+
+def fetch_pages():
+    churches = []
+
+    def get_path(pageid):
+        return f"data/reliwiki/html/{pageid}.html"
+
+    for church in Knead("data/reliwiki/pages.json").data():
+        pageid = church["pageid"]
+        path = get_path(pageid)
+
+        if Path(path).exists():
+            print(f"{path} exists, skipping")
+            continue
+
+        url = f"http://reliwiki.nl/?curid={pageid}"
+        print(f"Getting {url}")
+        req = requests.get(url)
+
+        with open(path, "w") as f:
+            f.write(req.text)
+            print(f"Saved {path}")
+
+        print("Sleep for 3 second")
+        sleep(3)
+
+def parse_page(path):
+    print(f"Parsing {path}")
+
+    with open(path) as f:
+        soup = BeautifulSoup(f.read(), "lxml")
+
+    # I guess the first table in mw-content-text is always the infobox
+    table = soup.select_one("#mw-content-text table")
+    infobox = {
+        "coordinates" : None,
+        "pageid" : Path(path).stem,
+        "rijksmonument" : []
+    }
+
+    for tr in table.select('tr[valign="top"]'):
+        td = tr.select("td")
+        key = td[0].get_text().strip()
+        val = td[1].get_text().strip()
+        infobox[key] = val
+
+    # Extract external references
+    for a in table.select("a"):
+        href = a.get("href")
+
+        if RMM_ID.match(href):
+            matches = RMM_ID.findall(href)
+            infobox["rijksmonument"].append(matches[0])
+
+    # Extract geocoordinates
+    el_mapdata = soup.select_one("#map_leaflet_1 .mapdata")
+
+    if el_mapdata:
+        mapdata = json.loads(el_mapdata.get_text())
+
+        if "locations" in mapdata and len(mapdata["locations"]) > 0:
+            loc = mapdata["locations"][0]
+            infobox["coordinates"] = f"{loc['lat']},{loc['lon']}"
+
+    infobox["rijksmonument"] = ",".join(infobox["rijksmonument"])
+
+    return infobox
 
 def parse_pages():
     churches = []
@@ -50,9 +125,18 @@ def parse_pages():
         fieldnames = ["pageid", "name", "address", "place", "pagetitle"]
     )
 
+def process_pages():
+    churches = []
+
+    for path in Path("data/reliwiki/html").glob("*.html"):
+        data = parse_page(path)
+        churches.append(data)
+
+    Knead(churches).write("data/reliwiki/churches_data.csv")
+
 def scrape_pages():
     churches = []
-    api = AllPages("http://reliwiki.nl/api.php")
+    api = AllPages(API_ENDPOINT)
 
     for page in api.iterate_pages():
         print(page["title"])
@@ -60,4 +144,4 @@ def scrape_pages():
 
     Knead(churches).write("data/reliwiki/pages.json")
 
-create_catalog()
+process_pages();
